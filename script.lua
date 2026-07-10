b6c7dacbac49487a48cf3aeced68806178c44940
Jackbow19 <ihsanfadhil456@gmail.com>
2026-07-10 16:03:21 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -104,9 +104,9 @@ _G.FishItConfig = _G.FishItConfig or {
         },
     },
     ["Enchant"] = {
-        ["Auto Enchant"] = false,
-        ["Roll Enchant"] = false, -- true = Roll until get TOP 1 enchant only (first in list)
-        ["Evolved Roll Enchant"] = false, -- true = Roll Evolved Stone until get ANY evolved-only enchant from list, then fallback to regular stone
+        ["Auto Enchant"] = true,
+        ["Roll Enchant"] = true, -- true = Roll until get TOP 1 enchant only (first in list)
+        ["Evolved Roll Enchant"] = true, -- true = Roll Evolved Stone until get ANY evolved-only enchant from list, then fallback to regular stone
         ["Enchant List"] = {
             "SECRET Hunter",
             "Mutation Hunter III",
