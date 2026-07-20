0f88aa7aa7b181b6f6ca6a5edc80adea21234ace
Jackbow19 <ihsanfadhil456@gmail.com>
2026-07-21 06:09:37 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -45,6 +45,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Auto Element Rod"] = false,
         ["Auto Element Rod 2"] = true,
         ["Auto Diamond Rod"] = false,
+        ["Auto Aetherion Bait"] = true,
         ["Unlock Ancient Ruin"] = false,
         ["Allowed Sacrifice"] = {
             "Gladiator Shark",
