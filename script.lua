802413f3aa5e73a1b1c1bf12dc3b726503701455
Jackbow19 <ihsanfadhil456@gmail.com>
2026-07-29 14:30:56 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -21,7 +21,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Auto Trade"] = {
         ["Enabled"] = true,
-        ["Whitelist Username"] = {"vcdagrbv4","nhmnhte6"},
+        ["Whitelist Username"] = {"nbhgfbfg3","xcasceeg4"},
         ["Category Fish"] = {
             "Secret",
         },
