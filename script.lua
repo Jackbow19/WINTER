7197bc5bb34ed8e81e236c2d12af53ad195ca5dc
Jackbow19 <ihsanfadhil456@gmail.com>
2026-08-24 10:49:48 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -100,7 +100,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Event"] = {
         ["Start Farm"] = true,
-        ["Minimum Rod"] = "Element Rod"",
+        ["Minimum Rod"] = "Ghostfinn Rod",
         ["Event List"] = {
             ["Elemental Event"] = true,
             ["Hacker Event"] = false,
