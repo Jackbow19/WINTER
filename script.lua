f9d8d1e11f0e117773bb5c4f91c47dd046feeb94
Jackbow19 <ihsanfadhil456@gmail.com>
2026-08-25 16:15:45 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -165,6 +165,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Endgame"] = "Element Rod",
     },
 
+    ["FPS Cap"] = 0,
     ["ExtremeFpsBoost"] = false,
     ["UltimatePerformance"] = false,
     ["Disable3DRender"] = false,
