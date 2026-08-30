5943f8f1271886a848428b1124d879d0ab1d1d76
Jackbow19 <ihsanfadhil456@gmail.com>
2026-08-30 19:20:28 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -167,7 +167,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
 
     ["FPS Cap"] = 0,
-    ["ExtremeFpsBoost"] = true,
+    ["ExtremeFpsBoost"] = false,
     ["UltimatePerformance"] = false,
     ["Disable3DRender"] = false,
     ["AutoRemovePlayer"] = true,
