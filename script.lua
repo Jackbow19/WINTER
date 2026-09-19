e079195fab2799173abe98bf41597c9f6138a319
Jackbow19 <ihsanfadhil456@gmail.com>
2026-09-19 12:42:25 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -97,7 +97,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Auto Use"] = true,
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Buy List"] = {
-            ["Lucky Totem"] = 100,
+            ["Luck Totem"] = 100,
         },
     },
     ["Event"] = {
